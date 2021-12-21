f39a4171f20416e1a809794f4dbd494faf3a131e
colocolov <colocolov.stas@gmail.com>
2021-12-21 13:35:42 +0200

    start projext. add header

---
+++ difa/js/main.js
@@ -0,0 +1,95 @@
+// accordeon
+document.addEventListener("DOMContentLoaded", () => {
+  const accordeons = document.querySelectorAll(".accordeon__item");
+
+  accordeons.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      const title = self.querySelector(".accordeon__name");
+      const content = self.querySelector(".accordeon__decription");
+
+      self.classList.toggle("active");
+    });
+  });
+});
+// ----- END
+
+// слайдер на главной
+const headerSlider = new Swiper(".tophead-slider", {
+  // loop: true,
+  autoplay: {
+    //пауза между прокруткой
+    delay: 3000,
+    //закончить на последнем слайде
+    // stopOnLastSlide: false,
+    //отключить после ручного переключения
+    // disableOnInteraction: false,
+  },
+  //скорость переключения слайдов
+  speed: 800,
+  // навигация по стрелкам
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    // disabledClass: "stories-button__unactive",
+    clickable: true,
+    // для ппрвильного направления
+  },
+  // буллеты
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  //эффект перехода слайда (только если показ по 1-му слайду)
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  // показ кол-ва слайдов (работает, когда откл effect: "fade")
+  slidesPerView: 3.6,
+  // расстояние между слайдами
+  spaceBetween: 40,
+  //отложенная загрузка:
+  //отключаем презагрузку картинок
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: false,
+    loadPrevNext: false,
+  },
+  // переключение при клике на слайд
+  slideToClickedSlide: true,
+  // отключение прокрутки при наведении мыши
+  on: {
+    init() {
+      this.el.addEventListener("mouseenter", () => {
+        this.autoplay.stop();
+      });
+
+      this.el.addEventListener("mouseleave", () => {
+        this.autoplay.start();
+      });
+    },
+  },
+  //
+});
+//----- END
+
+// слайдер отзывов
+var reviewsSlider = new Swiper(".reviews__sliders", {
+  loop: true,
+  navigation: {
+    nextEl: ".reviews-button--right",
+    prevEl: ".reviews-button--left",
+    clickable: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+//----- END
+
+//# sourceMappingURL=main.js.map
